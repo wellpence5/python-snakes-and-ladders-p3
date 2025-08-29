20738bc6a02553a6c5f921ded2376189749bacd1
wellpence5 <alvinmuthomi22@gmai.com>
2025-08-29 11:55:35 +0300

    Fixed main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-# We bring in everyones functions from other tasks
+#This is the link to everyones work. Game runs from here
+# We bring in everyones functions from their respective tasks
 from database import (
     init_db,
     add_player,
@@ -17,13 +17,12 @@ from cli_interface import (
 from turn_manager import game_loop
 
 def main():
-    #this is the entry point of the game.
     init_db() #make sure database actually still works
-    while True:#min loop to keeps showing the menu until the user chooses Exit
+    while True:#made a loop to keeps showing the menu until the user chooses Exit
         # check if there’s already a saved unfinished game
         active_game = get_active_game()
         if active_game:
-            #if a saved game exists, ask the player what to do with it
+            #if a saved game exists,ask the gamer what to do with it
             choice = choose_resume_or_new()
             if choice == "1":
                 # Resume the saved game
@@ -34,7 +33,7 @@ def main():
             elif choice == "2":
                 #delete the old game and start fresh
                 delete_game(active_game["id"])
-                print("Old game gone to make room. Gonna add more saves next update")
+                print("Old game gone to make room. Gonna add more saves next update Inshallah")
         #show the main menu(only if no active game or it was deleted)
         choice = main_menu()
         if choice == "1":
@@ -42,25 +41,20 @@ def main():
             try:
                 num_players = int(input("Enter number of players: "))
             except ValueError:
-                print("Try putting a number instead.")
+                print("Try putting a number(integer) instead.")
                 continue
-            #getting names
-            names = get_player_names(num_players)
-            #put each player to the database
-            players = [add_player(name) for name in names]
-            #create a new game record in the database
-            game = create_game()
-            #start the actual gameplay loop
-            game_loop(players, game["id"])
+            names = get_player_names(num_players)#getting names
+            players = [add_player(name) for name in names]#put each player to the database
+            game = create_game()#create a new game record in the database
+            game_loop(players, game["id"])#start the actual gameplay loop
         elif choice == "2":
             #view Leaderboard
             show_leaderboard()
         elif choice == "3":
             print("Goodbye! Give us a five star rating on play store for good luck :P")
             break
-
         else:
-            print("Oops XuX.You had 3 choices.")
+            print("Error occured .You had 3 choices.")
 
 if __name__ == "__main__":
     main()
