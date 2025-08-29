1070bc2977f19a577ea13d8e17184cba227538f4
wellpence5 <alvinmuthomi22@gmai.com>
2025-08-29 11:57:18 +0300

    cli_interface ya

--- cli_interface.py
+++ cli_interface.py
@@ -1,14 +1,18 @@
 from database import get_leaderboard
 
 def main_menu():
+    print("-----------------------------------------------------------")
     print("Snakes & Ladders :D")
     print("1. Start New Game")
     print("2. View Leaderboard")
     print("3. Exit")
+    print("-----------------------------------------------------------")
     choice = input("Choose an option (1-3): ").strip()
     #validation
     while choice not in {"1", "2", "3"}:
+        print("-----------------------------------------------------------")
         print("Oopss!. Please enter 1, 2, or 3.")
+        print("-----------------------------------------------------------")
         choice = input("Choose an option (1-3): ").strip()
     return choice
 
@@ -43,9 +47,11 @@ def show_leaderboard():
 
 def choose_resume_or_new():
     #Special menu shown when an unfinished game exists.
+    print("-----------------------------------------------------------")
     print("You have an unfinished game!")
     print("1. Resume it")
     print("2. Delete it and start afresh")
+    print("-----------------------------------------------------------")
     choice = input("Choose an option (1-2): ").strip()
     while choice not in {"1", "2"}:
         print("Invalid choice. Please enter 1 or 2.")
